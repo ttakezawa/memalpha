cd8eb0d1d25fdb068b31e715e528387fe1576dc6
Tomohiro Takezawa <ttakezawa@users.noreply.github.com>
2017-02-12 11:50:40 +0900

    Refactor benchmark

--- bench_test.go
+++ bench_test.go
@@ -1,54 +1,59 @@
 package memalpha
 
-import "testing"
+import (
+	"bytes"
+	"testing"
+)
 
-func benchmarkSet(b *testing.B, c *Client) {
-	key := "foo"
-	value := []byte("bar")
-	b.SetBytes(int64(len(key) + len(value)))
+func BenchmarkGetSmall(b *testing.B) { benchmarkGet(b, 5, 5) }
+func BenchmarkGetLarge(b *testing.B) { benchmarkGet(b, 250, 500*1024) }
+func BenchmarkSetSmall(b *testing.B) { benchmarkSet(b, 5, 5) }
+func BenchmarkSetLarge(b *testing.B) { benchmarkSet(b, 250, 500*1024) }
+
+func benchmarkGet(b *testing.B, keySize int, valueSize int) {
+	key := string(bytes.Repeat([]byte("A"), keySize))
+	value := bytes.Repeat([]byte("A"), valueSize)
+
+	memd := newServer()
+	if err := memd.Start(); err != nil {
+		b.Skipf("skipping test; couldn't start memcached: %s", err)
+	}
+	defer memd.Shutdown()
+
+	if err := memd.client.Set(key, value); err != nil {
+		b.Skipf("skipping test; couldn't set(%s, %s) = %+v", key, value, err)
+	}
+
+	b.SetBytes(int64(keySize + valueSize))
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		if err := c.Set(key, value); err != nil {
+		if _, _, err := memd.client.Get(key); err != nil {
 			b.Fatal(err)
 		}
 	}
 	b.StopTimer()
 }
 
-func benchmarkSetGet(b *testing.B, c *Client) {
-	key := "foo"
-	value := []byte("bar")
-	b.SetBytes(int64(len(key) + len(value)))
+func benchmarkSet(b *testing.B, keySize int, valueSize int) {
+	key := string(bytes.Repeat([]byte("A"), keySize))
+	value := bytes.Repeat([]byte("A"), valueSize)
+
+	memd := newServer()
+	if err := memd.Start(); err != nil {
+		b.Skipf("skipping test; couldn't start memcached: %s", err)
+	}
+	defer memd.Shutdown()
+
+	if err := memd.client.Set(key, value); err != nil {
+		b.Skipf("skipping test; couldn't set(%s, %s) = %+v", key, value, err)
+	}
+
+	b.SetBytes(int64(keySize + valueSize))
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		if err := c.Set(key, value); err != nil {
-			b.Fatal(err)
-		}
-		if _, _, err := c.Get(key); err != nil {
+		if err := memd.client.Set(key, value); err != nil {
 			b.Fatal(err)
 		}
 	}
 	b.StopTimer()
-}
-
-func BenchmarkSet(b *testing.B) {
-	memd := newServer()
-	err := memd.Start()
-	if err != nil {
-		b.Skipf("skipping test; couldn't start memcached: %s", err)
-	}
-	defer memd.Shutdown()
-
-	benchmarkSet(b, memd.client)
-}
-
-func BenchmarkSetGet(b *testing.B) {
-	memd := newServer()
-	err := memd.Start()
-	if err != nil {
-		b.Skipf("skipping test; couldn't start memcached: %s", err)
-	}
-	defer memd.Shutdown()
-
-	benchmarkSetGet(b, memd.client)
 }
