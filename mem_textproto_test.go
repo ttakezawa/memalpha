da7d4c3c60624128b6eb89850295f93d1e7fd213
Tomohiro Takezawa <ttakezawa@users.noreply.github.com>
2017-02-14 06:47:40 +0900

    Improve test coverage

--- mem_textproto_test.go
+++ mem_textproto_test.go
@@ -3,14 +3,22 @@ package memalpha
 import (
 	"bufio"
 	"bytes"
+	"fmt"
 	"io"
 	"io/ioutil"
+	"net"
 	"strconv"
 	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+type errorWriter struct{ error }
+
+func (e errorWriter) Write(p []byte) (int, error) {
+	return 0, e.error
+}
 
 func newFakedClient(response string, requestWriter io.Writer) *Client {
 	return &Client{rw: bufio.NewReadWriter(
@@ -51,11 +59,31 @@ func TestReplyError(t *testing.T) {
 	assert.Equal(t, ErrReplyError, err)
 }
 
+func TestMalformedGetResponse(t *testing.T) {
+	{
+		c := newFakedClient("foobar", ioutil.Discard)
+		_, _, err := c.Get("foo")
+		got := err.Error()
+		expected := "malformed response"
+		comparison := func() bool {
+			return strings.Contains(got, expected)
+		}
+		assert.Condition(t, comparison, fmt.Sprintf("%q should have prefix %q", got, expected))
+	}
+}
+
 func TestMalformedStatsResponse(t *testing.T) {
 	{
 		c := newFakedClient("foobar", ioutil.Discard)
 		_, err := c.Stats()
 		assert.Equal(t, ProtocolError("malformed stats response"), err)
+	}
+
+	{
+		expected := net.UnknownNetworkError("test")
+		c := newFakedClient("foobar", errorWriter{expected})
+		_, err := c.Stats()
+		assert.Equal(t, expected, err)
 	}
 }
 
