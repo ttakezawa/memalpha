db80a97474407af3237dab64537ff31a8a9e282f
Tomohiro Takezawa <ttakezawa@users.noreply.github.com>
2017-02-14 22:10:10 +0900

    Add benchmark with mock server

--- bench_test.go
+++ bench_test.go
@@ -1,14 +1,59 @@
 package memalpha
 
 import (
+	"bufio"
 	"bytes"
+	"fmt"
+	"io/ioutil"
 	"testing"
 )
 
-func BenchmarkGetSmall(b *testing.B) { benchmarkGet(b, 5, 5) }
-func BenchmarkGetLarge(b *testing.B) { benchmarkGet(b, 250, 1023*1024) }
-func BenchmarkSetSmall(b *testing.B) { benchmarkSet(b, 5, 5) }
-func BenchmarkSetLarge(b *testing.B) { benchmarkSet(b, 250, 1023*1024) }
+func BenchmarkGetStubSmall(b *testing.B) { benchmarkGetStub(b, 5, 5) }
+func BenchmarkGetStubLarge(b *testing.B) { benchmarkGetStub(b, 250, 1023*1024) }
+func BenchmarkGetSmall(b *testing.B)     { benchmarkGet(b, 5, 5) }
+func BenchmarkGetLarge(b *testing.B)     { benchmarkGet(b, 250, 1023*1024) }
+func BenchmarkSetSmall(b *testing.B)     { benchmarkSet(b, 5, 5) }
+func BenchmarkSetLarge(b *testing.B)     { benchmarkSet(b, 250, 1023*1024) }
+
+type repeatReader struct {
+	response []byte
+	offset   int
+}
+
+func (r *repeatReader) Read(p []byte) (int, error) {
+	r.offset = r.offset % len(r.response)
+	n := copy(p, r.response[r.offset:])
+	r.offset += n
+	return n, nil
+}
+
+func newRepeatReader(response []byte) *repeatReader {
+	return &repeatReader{response, 0}
+}
+
+// benchmark with mock server
+func benchmarkGetStub(b *testing.B, keySize int, valueSize int) {
+	key := string(bytes.Repeat([]byte("A"), keySize))
+	value := bytes.Repeat([]byte("A"), valueSize)
+
+	response := fmt.Sprintf("VALUE %s 0 %d\r\n%s\r\nEND\r\n", key, len(value), string(value))
+	c := &Client{
+		rw: bufio.NewReadWriter(
+			bufio.NewReader(newRepeatReader([]byte(response))),
+			bufio.NewWriter(ioutil.Discard),
+		),
+	}
+
+	b.SetBytes(int64(keySize + valueSize))
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, _, err := c.Get(key)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+	b.StopTimer()
+}
 
 func benchmarkGet(b *testing.B, keySize int, valueSize int) {
 	key := string(bytes.Repeat([]byte("A"), keySize))
